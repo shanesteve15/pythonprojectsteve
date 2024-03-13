3fa06e5b24d882bf234eea3750e2018c18f6519b
shanesteve15 <shanekanonga430@gmail.com>
2024-03-13 01:47:02 -0400

    updates

--- shanepython.py
+++ shanepython.py
@@ -1,1 +1,50 @@
+#operators
 x=5
+x+=3 #this is like saying x=x+3
+x=x+3
+x*=4 #x=x*4
+print("my answer is",x)
+
+#== sign maens equal to
+y=20
+y==34
+print(y)
+
+
+#!= not equal to
+p=5
+q=4
+print(p!=q)
+
+#conditional statements
+age= 50
+if age>=18:
+    print("you are an adult")
+
+
+# if...elif statements
+if age>18:
+    print("adult")
+elif age<18:
+    print("you are a child")
+
+
+#if..elif..else statements
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+
+#using the and satements
+president_age=20
+nationality="kenyan"
+if president_age >=18 and nationality=="kenyan":
+    print("you are a successful candidate")
+else:
+   print("you cannot be a candidaate")
