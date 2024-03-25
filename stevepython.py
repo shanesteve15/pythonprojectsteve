89b01761a26ff5bc164df253009d9998d74124ec
shanesteve15 <shanekanonga430@gmail.com>
2024-03-25 03:41:45 -0400

    new work updates

--- stevepython.py
+++ stevepython.py
@@ -9,8 +9,7 @@ else:
 finally:
     print(" finished")
 
-#class is like a blueprint for creating objects
-
+#class is like a blueprint for creating object
 class Student:
       student_id = 0
       student_name = ""
@@ -21,8 +20,56 @@ class Student:
           self.student_age =student_age
       def display(self):
           print("Student details are:""Name:",self.student_name,   "Age:",self.student_age,   "Student ID:",self.student_id)
-student1 = Student(student_id="10",    student_name="John",   student_age="30")
+student1 = Student(  "10",    "John",  "30")
 student1.display()
+
+
+#Python Inheritance
+class person:
+    person_age = 0
+    person_name = ""
+    person_residence=""
+    def __init__(self, person_age, person_name, person_residence):
+        self.person_age = person_age
+        self.person_name = person_name
+        self.person_residence = person_residence
+    def print_info(self):
+        print("person_age:",self.person_age, "person_name:",self.person_name, "person_residence:",self.person_residence)
+
+class student(person):
+    uniform=""
+    def __init__(self,name,age,residence,uni):
+        super().__init__(name,age,residence)
+        self.uniform=uni
+        print("Uniform:",self.uniform)
+
+student1 = student("46", "kamau", "380", "blue")
+student1.print_info()
+
+
+#Polymorphism
+class animal:
+    limbs=4
+    eyes=2
+    def __init__(self,limbs,eyes):
+        self.limbs=limbs
+        self.eyes=eyes
+        print ("limbs:",self.limbs,"eyes:",self.eyes)
+    def speak(self):
+        pass
+class Dog(animal):
+    def speak(self):
+        print("I am a dog and i bark")
+class Cat(animal):
+    def speak(self):
+     print("I am a cat and i meow")
+dog1=Dog(limbs=4 , eyes=2)
+dog1.speak()
+cat1=Cat(limbs=4 , eyes=2)
+cat1.speak()
+
+
+
 
 
 
